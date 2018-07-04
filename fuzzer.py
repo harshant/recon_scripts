46ed1c0ad32b39d361765aebd91e18b60f2fe41b
h45h5h0t <harshcosmo@gmail.com>
2018-07-05 00:34:09 +0530

    little modification

--- fuzzer.py
+++ fuzzer.py
@@ -8,6 +8,10 @@ bad = '\033[91m[-]\033[0m'
 run = '\033[97m[~]\033[0m'
 
 
+import requests #importing module
+import argparse #importing module
+
+
 parser = argparse.ArgumentParser()
 parser.add_argument("-d", "--domain",help="Input a domain to recursively parse all javascript located in a page",action="store_true")
 parser.add_argument("-i", "--input", help="Input a: URL, file or folder. For folders a wildcard can be used (e.g. '/*.js').",required="True", action="store")
@@ -16,3 +20,7 @@ parser.add_argument("-b", "--burp",help="",action="store_true")
 parser.add_argument("-c", "--cookies",help="Add cookies for authenticated JS files",action="store", default="")
 parser.add_argument("-a","--header",help="Add authorization header",action="store",default="")
 args = parser.parse_args()
+
+
+#making header acceptable to requests.get() function
+he=args.header.split(":")
